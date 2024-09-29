03192ab939d7104814bf3678a4713b213e1ccac8
valitovgaziz <valitovgaziz@gmail.com>
2024-09-29 06:34:49 +0500

    Add LoginView into routing

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: () => import('../view/LoginView.vue')
     }
   ]
 })
