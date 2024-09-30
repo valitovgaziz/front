d2ef73dcf288dfc5111c047a166807cf9683e489
valitovgaziz <valitovgaziz@gmail.com>
2024-09-30 05:50:07 +0500

    Add the apolloclient to main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
-
 import './index.css'
 
-const app = createApp(App)
+import { createApolloClient } from './apolloClient'
+import { DefaultApolloClient } from '@vue/apollo-composable'
 
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, createApolloClient())
+  },
+  render: () => h(App)
+})
 app.use(createPinia())
 app.use(router)
 
